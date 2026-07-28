a5fd18a687a96751f01daf234aebf3ba70961e03
D4mial <147994190+D4mial@users.noreply.github.com>
2026-07-28 11:21:02 +0700

    Add output status widget implementation

---
+++ config/boards/shields/fine40/widgets/output_status.c
@@ -0,0 +1,97 @@
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+#include <zmk/endpoints.h>
+#include <zmk/events/endpoint_changed.h>
+#include <zmk/event_manager.h>
+
+#if IS_ENABLED(CONFIG_ZMK_BLE)
+#include <zmk/ble.h>
+#include <zmk/events/ble_active_profile_changed.h>
+#endif
+
+#include "output_status.h"
+
+static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
+
+struct output_status_state {
+    struct zmk_endpoint_instance selected_endpoint;
+#if IS_ENABLED(CONFIG_ZMK_BLE)
+    int active_profile_index;
+    bool active_profile_connected;
+    bool active_profile_bonded;
+#endif
+};
+
+static void set_status_symbol(lv_obj_t *label, struct output_status_state state) {
+    char text[16] = {};
+
+    switch (state.selected_endpoint.transport) {
+    case ZMK_TRANSPORT_USB:
+        snprintf(text, sizeof(text), LV_SYMBOL_USB);
+        break;
+#if IS_ENABLED(CONFIG_ZMK_BLE)
+    case ZMK_TRANSPORT_BLE:
+        if (state.active_profile_bonded) {
+            if (state.active_profile_connected) {
+                snprintf(text, sizeof(text), LV_SYMBOL_BLUETOOTH "%d", state.active_profile_index);
+            } else {
+                snprintf(text, sizeof(text), LV_SYMBOL_CLOSE "%d", state.active_profile_index);
+            }
+        } else {
+            snprintf(text, sizeof(text), LV_SYMBOL_SETTINGS "%d", state.active_profile_index);
+        }
+        break;
+#endif
+    default:
+        snprintf(text, sizeof(text), LV_SYMBOL_CLOSE);
+        break;
+    }
+
+    lv_label_set_text(label, text);
+}
+
+static void output_status_update_cb(struct output_status_state state) {
+    struct zmk_widget_output_status *widget;
+    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) {
+        set_status_symbol(widget->obj, state);
+    }
+}
+
+static struct output_status_state output_status_get_state(const zmk_event_t *eh) {
+    return (struct output_status_state){
+        .selected_endpoint = zmk_endpoints_selected(),
+#if IS_ENABLED(CONFIG_ZMK_BLE)
+        .active_profile_index = zmk_ble_active_profile_index(),
+        .active_profile_connected = zmk_ble_active_profile_is_connected(),
+        .active_profile_bonded = !zmk_ble_active_profile_is_open(),
+#endif
+    };
+}
+
+ZMK_DISPLAY_WIDGET_LISTENER(widget_output_status, struct output_status_state, output_status_update_cb,
+                             output_status_get_state)
+
+ZMK_SUBSCRIPTION(widget_output_status, zmk_endpoint_changed);
+#if IS_ENABLED(CONFIG_ZMK_BLE)
+ZMK_SUBSCRIPTION(widget_output_status, zmk_ble_active_profile_changed);
+#endif
+
+int zmk_widget_output_status_init(struct zmk_widget_output_status *widget, lv_obj_t *parent) {
+    widget->obj = lv_label_create(parent);
+
+    lv_obj_set_style_text_font(widget->obj, &lv_font_montserrat_28, 0);
+    lv_label_set_text(widget->obj, LV_SYMBOL_USB);
+
+    sys_slist_append(&widgets, &widget->node);
+
+    widget_output_status_init();
+
+    return 0;
+}
+
+lv_obj_t *zmk_widget_output_status_obj(struct zmk_widget_output_status *widget) {
+    return widget->obj;
+}
